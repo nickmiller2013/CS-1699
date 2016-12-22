0244a1a8cd264642ee0216f7e6998e9cd23f0375
Nick Miller <nam99@pitt.edu>
2016-12-22 11:28:25 -0500

    Update WordCount.py

--- Tiny-Google/WordCount.py
+++ Tiny-Google/WordCount.py
@@ -1,8 +1,1 @@
-#text_file = sc.textFile("hdfs://...")
-#counts = text_file.flatMap(lambda line: line.split(" ")) \
-#             .map(lambda word: (word, 1)) \
-#             .reduceByKey(lambda a, b: a + b)
-#counts.saveAsTextFile("hdfs://...")
 
-
-##Template WordCount 
